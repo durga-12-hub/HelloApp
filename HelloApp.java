7176c370d34c04aa6f2cb7a888edc7dcf7f7cc56
durga-12-hub <db543952@gmail.com>
2026-04-02 02:09:22 +0530

    Initial commit for HelloApp

---
+++ HelloApp.java
@@ -0,0 +1,8 @@
+
+
+public class HelloApp {
+    public static void main(String[] args) {
+        // The instruction uses a labeled parameter style, but standard Java is:
+        System.out.println("Hello, World!");
+    }
+}
