39f008c871e6ff11f88dbe6ac7f46ab1e8840604
durga-12-hub <db543952@gmail.com>
2026-04-02 02:25:02 +0530

    UC4: Implement multiple arguments with StringBuilder and loops

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,24 @@
 
+
 public class HelloApp {
     public static void main(String[] args) {
-        // 1. Set a default name
         String name = "World";
 
-        // 2. Check if the user provided an argument
-        // .length tells us how many words you typed after the command
         if (args.length > 0) {
-            name = args[0]; // Overwrite "World" with the input name
+            // StringBuilder is more efficient than String for joining text
+            StringBuilder nameBuilder = new StringBuilder();
+            
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                
+                // Add a comma and space if it's not the last name
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
         }
 
-        // 3. Output the greeting
         System.out.println("Hello, " + name + "!");
     }
 }
